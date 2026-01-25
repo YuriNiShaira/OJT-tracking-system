27eb857bd76bf888fee04021f3dd20a8d3190672
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-01-26 00:50:45 +0800

    added notif route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,4 +16,10 @@ urlpatterns = [
     # Dashboard Stats
     path('dashboard/company-stats/', views.company_dashboard_stats, name='company-stats'),
     path('dashboard/student-stats/', views.student_dashboard_stats, name='student-stats'),
+
+    #Notif
+    path('notifications/', views.NotificationList.as_view(), name='notifications-list'),
+    path('notifications/<int:pk>/read/', views.mark_notification_read, name='mark-notification-read'),
+    path('notifications/read-all/', views.mark_all_notifications_read, name='mark-all-notifications-read'),
+    path('notifications/stats/', views.notification_stats, name='notification-stats'),
 ]
